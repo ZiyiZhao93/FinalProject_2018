f633f82f4d537d04258d285dd0df51259e1805cc
ZiyiZhao93 <31828737+ZiyiZhao93@users.noreply.github.com>
2018-03-29 21:57:43 -0400

    start of final project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 import * as d3 from 'd3';
 import "./style/main.css";
 
+import {parse, parse2, parseStation, fetchCsv} from '../utils';
+
+import Histogram, {activityHistogram} from './components/Histogram';
+
+
+d3.csv('/data/hubway_trips_reduced.csv', parse, function(err,trips){
+	const t0 = d3.min(trips, d => d.t0);
+		const t1 = d3.max(trips, d => d.t1);
+
+		console.log(trips);
+/*
+
+		d3.select('#mainbox')
+			.datum(trips)
+			.each(activityHistogram);*/
+
+
+
+		
+
+});
+
+
+		
+
+
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
